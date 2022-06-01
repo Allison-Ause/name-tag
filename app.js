35af8a02913c8343661d6459238cdbeb49ad3dee
Allison Ause <allison.ause@gmail.com>
2022-05-31 17:05:28 -0700

    Validated console.log of the name input. Confirmed on console of livepage

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // grab DOM elements
-
+const nameInput = document.getElementById('name-input');
+console.log(nameInput);
 // set event listeners
     // get info from user input
     // use user input to update state
